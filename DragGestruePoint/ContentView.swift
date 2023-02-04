3640f6b4da1cbf342d45a8a54004afc6ecdf8351
sasasan03 <sako.hiro62@gmail.com>
2023-02-04 13:41:59 +0900

    Viewの座標を知るために使用

--- DragGestruePoint/ContentView.swift
+++ DragGestruePoint/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var position:CGSize = CGSize(width: 0, height: 0)
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged{ value in
+                self.position = CGSize(
+                    width: value.startLocation.x + value.translation.width,
+                    height: value.startLocation.y + value.translation.height
+                )
+            }
+            .onEnded{ value in
+                self.position = CGSize(
+                    width: value.startLocation.x + value.translation.width,
+                    height: value.startLocation.y + value.translation.height
+                )
+            }
+    }
+        
+    
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("x: \(position.width)")
+            Text("y:\(position.height)")
+            Image(systemName: "cross.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .position(x: position.width, y: position.height)
+                .gesture(drag)
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
