0a609875fc39196438a4c9f2161a7e8e86a763b3
sasasan03 <sako.hiro62@gmail.com>
2023-02-04 13:35:09 +0900

    Initial Commit

---
+++ DragGestruePoint/DragGestruePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestruePointApp.swift
+//  DragGestruePoint
+//
+//  Created by sako0602 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct DragGestruePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
