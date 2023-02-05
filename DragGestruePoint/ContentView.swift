9ecbc3b1f92ca78f418df50430198ccbecbef9c1
sasasan03 <sako.hiro62@gmail.com>
2023-02-05 22:36:13 +0900

    GeometryReaderを使ってコンテナ（親View ）の中央に画像を配置

--- DragGestruePoint/ContentView.swift
+++ DragGestruePoint/ContentView.swift
@@ -6,39 +6,44 @@
 //
 
 import SwiftUI
-
+//課題：中心に持ってきたい
 struct ContentView: View {
     @State var position:CGSize = CGSize(width: 0, height: 0)
-    
-    var drag: some Gesture {
-        DragGesture()
-            .onChanged{ value in
-                self.position = CGSize(
-                    width: value.startLocation.x + value.translation.width,
-                    height: value.startLocation.y + value.translation.height
-                )
-            }
-            .onEnded{ value in
-                self.position = CGSize(
-                    width: value.startLocation.x + value.translation.width,
-                    height: value.startLocation.y + value.translation.height
-                )
-            }
-    }
-        
-    
+     
     var body: some View {
         
-        
-        VStack {
-            Text("x: \(position.width)")
-            Text("y:\(position.height)")
-            Image(systemName: "cross.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 30, height: 30)
-                .position(x: position.width, y: position.height)
-                .gesture(drag)
+        GeometryReader { geometry in
+            let drag = DragGesture()
+                    .onChanged{ value in
+                        print("***1posi", position)
+                        self.position = value.translation
+                        print("***2posi", position)
+                    }
+                    .onEnded{ value in
+                        print(">>>1posi", position)
+                        self.position = CGSize(width: 0, height: 0)
+//                        self.position = CGSize(//現在地をプロパティに渡す
+//                            width: value.startLocation.x + value.translation.width,
+//                            height: value.startLocation.y + value.translation.height
+//                        )//初期の値に追加されていく。
+                        print(">>>2posi", position)
+                    }
+            
+            VStack {
+                Text("x: \(position.width)")
+                Text("y:\(position.height)")
+
+                    Image(systemName: "cross.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .position( //Viewの中央に画像を配置。移動分の座標は + position.[w]or[hight]
+                                   //geometryで
+                            x: geometry.size.width / 2 + position.width,
+                            y: geometry.size.height / 2 + position.height
+                        )
+                        .gesture(drag)
+            }
         }
     }
 }
